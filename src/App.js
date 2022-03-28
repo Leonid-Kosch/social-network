068e50064a0ec1fcf02cfa368b68b9966a342508
Leonid <leonidkoscheev984@gmail.com>
2022-03-28 15:46:15 +0200

    Set nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header.js'; 
+import Nav from './components/Nav/Nav.js';
 
 function App() {
   return (
     <div className="Wrapper">
         <Header />
+        <Nav />
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import './Nav.css';
+
+function Nav () {
+    return(
+        <div className="nav">
+            <a>Profile</a>
+            <a>Messages</a>
+            <a>Users</a>
+        </div>
+    );
+}
+
+export default Nav;
