19d60e291f3a7b668b12e1a8dcb1c7d949251cb3
Leonid <leonidkoscheev984@gmail.com>
2022-04-13 16:47:44 +0200

    настроили маршуртирацию профиля

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Dialogs from './components/Dialogs/Dialogs.js';
 import Header from './components/header/Header.js'; 
 import Nav from './components/Nav/Nav.js';
 import Profile from './components/Profile/Profile.js';
+import {BrowserRouter,Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="wrapper">
+      <BrowserRouter>
         <Header />
         <Nav />
         <div className='main-content'>
-          {/* <Profile /> */}
-          <Dialogs />
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
         </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,12 @@
 import './Nav.css';
+import {NavLink} from 'react-router-dom';
 
 function Nav () {
     return(
         <div className="nav">
-            <a>Профиль</a>
-            <a>Сообщения</a>
-            <a>Пользователи</a>
+            <NavLink to='/profile'> Профиль</NavLink>
+            <NavLink to='/dialogs'> Сообщения</NavLink>
+            <NavLink to='/user'> Пользовтаели</NavLink>
         </div>
     );
 }
