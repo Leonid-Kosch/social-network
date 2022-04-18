680df02e75ee80b640845ab969ec728eb871770c
Leonid <leonidkoscheev984@gmail.com>
2022-04-18 14:59:25 +0200

    добавили менеджер сообщений

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,8 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import './Dialogs.css';
 import Messages from './Messages/Messages';
 function Dialogs () {
     return(
         <div className='dialogs'>
+            <div className='dialog__list'>
+                <NavLink to='/'>Andrey</NavLink>
+                <NavLink to='/'>Leonid</NavLink>
+                <NavLink to='/'>Dima</NavLink>
+            </div>
             <Messages />
         </div>
     );
--- src/components/Dialogs/Messages/Messages.js
+++ src/components/Dialogs/Messages/Messages.js
@@ -4,11 +4,18 @@ import Me from '../../../image/me.jpg'
 function Messages () {
     return(
         <div className="messages">
-           <Message Img={Me} Name='Leonid' Text='Ja na zavtrak jel chto to'/> 
-           <Message Img={Me} Name='Net' Text='Ja na zavtrak jel chto to'/> 
-           <Message Img={Me} Name='Wolk227' Text='Ja na zavtrak jel chto to'/> 
-           <Message Img={Me} Name='Aleks' Text='Ja na zavtrak jel chto to'/> 
-           <Message Img={Me} Name='Ololo' Text='Ja na zavtrak jel chto to'/> 
+            <div className="messages__managment">
+                <textarea placeholder="Сообщение"></textarea>
+                <button><i class="fa-solid fa-angle-up"></i></button>
+            </div>
+           <Message Img={Me} Name='Leonid' Text='test'/> 
+           <Message Img={Me} Name='Net' Text='2'/> 
+           <Message Img={Me} Name='Wolk227' Text='4'/> 
+           <Message Img={Me} Name='Aleks' Text='6'/> 
+           <Message Img={Me} Name='Ololo' Text='8'/>
+           <Message Img={Me} Name='Ololo' Text='8'/> 
+           <Message Img={Me} Name='Ololo' Text='8'/> 
+           <Message Img={Me} Name='Ololo' Text='8'/>  
         </div>
     );
 }
