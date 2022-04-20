f61f34e40f79a47a14fc360e4dffb63b04c1e4ca
Leonid <leonidkoscheev984@gmail.com>
2022-04-20 16:41:50 +0200

    добавлено полее для ввода и кнопка отправки

--- src/components/Dialogs/Messages/Messages.js
+++ src/components/Dialogs/Messages/Messages.js
@@ -4,19 +4,20 @@ import Me from '../../../image/me.jpg'
 function Messages () {
     return(
         <div className="messages">
-            <div className="messages__managment">
+           <Message Img={Me} Name='Леонид' Text='test'/> 
+           <Message Img={Me} Name='Андрей' Text='Hello world'/> 
+           <Message Img={Me} Name='Саша' Text='56'/> 
+           <Message Img={Me} Name='Алексей' Text='6'/> 
+           <Message Img={Me} Name='Маша' Text='8'/>
+           <Message Img={Me} Name='Мария' Text='8'/> 
+           <Message Img={Me} Name='Катя' Text='8'/> 
+           <Message Img={Me} Name='Александр' Text='8'/>  
+           <div className="messages__managment">
                 <textarea placeholder="Сообщение"></textarea>
                 <button><i class="fa-solid fa-angle-up"></i></button>
             </div>
-           <Message Img={Me} Name='Leonid' Text='test'/> 
-           <Message Img={Me} Name='Net' Text='2'/> 
-           <Message Img={Me} Name='Wolk227' Text='4'/> 
-           <Message Img={Me} Name='Aleks' Text='6'/> 
-           <Message Img={Me} Name='Ololo' Text='8'/>
-           <Message Img={Me} Name='Ololo' Text='8'/> 
-           <Message Img={Me} Name='Ololo' Text='8'/> 
-           <Message Img={Me} Name='Ololo' Text='8'/>  
         </div>
+
     );
 }
 export default Messages;
